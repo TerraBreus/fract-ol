cd3ca542440c98af0f455f7059a50a7a2d990f92
Zacharia Ivanov <zivanov@f1r7s2.codam.nl>
2025-03-31 17:05:51 +0200

    update main for fract-ol

--- src/main.c
+++ src/main.c
@@ -5,63 +5,26 @@
 /*                                                      +:+                   */
 /*   By: zivanov <marvin@42.fr>                        +#+                    */
 /*                                                    +#+                     */
-/*   Created: 2025/02/26 17:01:56 by zivanov        #+#    #+#                */
-/*   Updated: 2025/02/27 14:19:00 by zivanov        ########   odam.nl        */
+/*   Created: 2025/03/31 13:37:43 by zivanov        #+#    #+#                */
+/*   Updated: 2025/03/31 15:30:39 by zivanov        ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mlx.h"
+#include "fractol.h"
 
-typedef struct	s_data
+int	main(int argc, char *argv[])
 {
-	void	*img;
-	char	*addr;
-	int	bits_per_pixel;
-	int	line_length;
-	int	endian;
-}		t_data;
+	t_fractal	fractal;
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char	*dst;
+	if (correct_input(argc, argv) == false)	//TODO
+		return (-1);
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel) / 8);
-	*(unsigned int*)dst = color;
-}
-
-void	draw_square(t_data *data, int x, int y, int color)
-{
-	int i;
-	int size;
-
-	size = 50;
-
-	i = -1;
-	while (++i < size)
-	{
-		my_mlx_pixel_put(data, x + i, y, color);
-		my_mlx_pixel_put(data, x, y + i, color);
-		my_mlx_pixel_put(data, x + size, y + i, color);
-		my_mlx_pixel_put(data, x + i, y + size, color);
-	}
-}
-
-int	main(void)
-{
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
-
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello World");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	establish_fractal_type();		//TODO
 	
-	draw_square(&img, 1920/2, 1080/2, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-
-	(void)mlx_win;
-	(void)img;
-	mlx_loop(mlx);
+	fractal_init(&fractal);			//TODO
+	fractal_render(&fractal);		//TODO
+	
+	check_events();
+	
 	return (0);
 }
