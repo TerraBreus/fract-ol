ac2d4a8cd4d5e77bdf373eeea3c4ccb058fc2d87
Zacharia Ivanov <zivanov@f1r7s1.codam.nl>
2025-04-03 16:12:28 +0200

    Added NULL checks for diversive use of the function

--- src/exit_utils.c
+++ src/exit_utils.c
@@ -6,19 +6,11 @@
 /*   By: zivanov <marvin@42.fr>                        +#+                    */
 /*                                                    +#+                     */
 /*   Created: 2025/04/01 16:57:43 by zivanov        #+#    #+#                */
-/*   Updated: 2025/04/03 13:00:42 by zivanov        ########   odam.nl        */
+/*   Updated: 2025/04/03 13:54:25 by zivanov        ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
-
-void	ft_freestr(char *str)
-{
-	if (str != NULL)
-		free(str);
-	//else
-	//	ft_putstr_fd("Trying to free NULL pointer\n", 2); //TODO
-}
 
 /* Plan to use this function whenever an error
 ** occurs and we need to free everything
@@ -28,12 +20,18 @@ void	ft_freestr(char *str)
 ** for exitting program at users command.
 */
 
-void	exit_program(t_fractal *fractal)	//TODO
+void	exit_program(t_fractal *fractal, int exit_status)
 {
-	mlx_destroy_image(fractal->mlx_ptr, fractal->img.mlx_img);
-	mlx_destroy_window(fractal->mlx_ptr, fractal->mlx_window);
-	mlx_destroy_display(fractal->mlx_ptr);
-	free(fractal->mlx_ptr);
-	ft_freestr(fractal->name);
-	exit(EXIT_FAILURE);
+	if (fractal->mlx_ptr != NULL)
+	{
+		if (fractal->img.mlx_img != NULL)
+			mlx_destroy_image(fractal->mlx_ptr, fractal->img.mlx_img);
+		if (fractal->mlx_window != NULL)
+			mlx_destroy_window(fractal->mlx_ptr, fractal->mlx_window);
+		mlx_destroy_display(fractal->mlx_ptr);
+		free(fractal->mlx_ptr);
+	}
+	if (fractal->name != NULL)
+		free(fractal->name);
+	exit(exit_status);
 }
