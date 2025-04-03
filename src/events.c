226f3a3dad3ef25622242d30436ea0fd93454e30
Zacharia Ivanov <zivanov@f1r7s1.codam.nl>
2025-04-03 19:32:49 +0200

    Added zoom function to mousewheel

--- src/events.c
+++ src/events.c
@@ -6,7 +6,7 @@
 /*   By: zivanov <marvin@42.fr>                        +#+                    */
 /*                                                    +#+                     */
 /*   Created: 2025/04/03 13:58:56 by zivanov        #+#    #+#                */
-/*   Updated: 2025/04/03 16:06:34 by zivanov        ########   odam.nl        */
+/*   Updated: 2025/04/03 19:31:21 by zivanov        ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,26 @@ int	close_on_destroy(t_fractal *fractal)
 	return (0);
 }
 
+int	ft_zoom(int key, int x, int y, t_fractal *fractal)
+{
+	x++;
+	y++;
+	if (key == 4)
+	{
+		fractal->zoom *= 0.9;
+		fractal_render(fractal);
+	}
+	if (key == 5)
+	{
+		fractal->zoom *= 1.1;
+		fractal_render(fractal);
+	}
+	return (0);
+}
+
 void	check_events(t_fractal *fractal)
 {
 	mlx_key_hook(fractal->mlx_window, close_on_escape, fractal);
 	mlx_hook(fractal->mlx_window, DESTROY_WINDOW, 1L << 0, close_on_destroy, fractal);
+	mlx_mouse_hook(fractal->mlx_window, ft_zoom, fractal);
 }
